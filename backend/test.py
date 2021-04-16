b9c32bf3fb5b9b114eda37ba878d1e4185ad5e12
Matteoo98 <matteo.molteni1998@gmail.com>
2021-04-16 11:38:42 +0200

    excel

--- backend/test.py
+++ backend/test.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+
+
     def test_something(self):
-        self.assertEqual(True, False)
+        #self.assertEqual(True, False)
+        
 
 
 if __name__ == '__main__':
